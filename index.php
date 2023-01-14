cf692029ce568d63b9bb7883ab5e69463d7e4c60
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-14 03:33:42 -0500

    decrease height

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ require_once 'scripts/pi-hole/php/gravity.php';
                     <h3 class="box-title">Speedtest results over last <?php echo htmlspecialchars($speedtestdays); ?></h3>
                 </div>
                 <div class="box-body">
-                    <div class="chart" style="height: 240px">
+                    <div class="chart" style="height: 180px">
                         <canvas id="speedOverTimeChart"></canvas>
                     </div>
                 </div>
