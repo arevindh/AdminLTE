4b4c58c7c68eec25341bcf11f2dd5e906a96ef43
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-19 23:24:19 -0500

    make disclaimer more obvious

--- settings.php
+++ settings.php
@@ -1535,7 +1535,7 @@ if (isset($_GET['tab']) && in_array($_GET['tab'], array('sysadmin', 'dns', 'piho
                                                     </div>
                                                 </div>
 
-                                                <h4>Update and/or Uninstall (BETA)</h4>
+                                                <h4>Update and/or Uninstall (HIGHLY EXPERIMENTAL)</h4>
                                                 <div class="form-group col-md-12">
                                                     <div>
                                                         <input type="checkbox" name="speedtestupdate" id="speedtestupdate" />
