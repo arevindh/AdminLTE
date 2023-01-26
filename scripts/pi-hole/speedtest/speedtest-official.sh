0baf7a898c32f3add550a0c37af256b710b44ef6
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-26 18:37:05 -0500

    if py test still installed, user wants to use it

--- scripts/pi-hole/speedtest/speedtest-official.sh
+++ scripts/pi-hole/speedtest/speedtest-official.sh
@@ -16,13 +16,11 @@ function nointernet(){
     exit 0
 }
 
-version=$(speedtest --version)
- 
-if [[ $version == *"Python"* ]]; then
+if [ -f /usr/local/bin/speedtest-cli ]; then
     if [[ "$serverid" =~ ^[0-9]+$ ]]; then
-        /usr/bin/speedtest -s $serverid --json --share --secure > /tmp/speedtest.log || nointernet
+        /usr/local/bin/speedtest-cli -s $serverid --json --share --secure > /tmp/speedtest.log || nointernet
     else
-        /usr/bin/speedtest --json --share --secure > /tmp/speedtest.log || nointernet
+        /usr/local/bin/speedtest-cli --json --share --secure > /tmp/speedtest.log || nointernet
     fi
 else 
     if [[ "$serverid" =~ ^[0-9]+$ ]]; then
@@ -38,7 +36,7 @@ if [[ -f "$FILE" ]]; then
     server_name=`cat /tmp/speedtest.log| jq -r '.server.name'`
     server_dist=0
 
-    if [[ $version == *"Python"* ]]; then
+    if [ -f /usr/local/bin/speedtest-cli ]; then
         download=`cat /tmp/speedtest.log| jq -r '.download' | awk '{$1=$1/1000/1000; print $1;}' | sed 's/,/./g' `
         upload=`cat /tmp/speedtest.log| jq -r '.upload' | awk '{$1=$1/1000/1000; print $1;}' | sed 's/,/./g'`
         isp=`cat /tmp/speedtest.log| jq -r '.client.isp'`
