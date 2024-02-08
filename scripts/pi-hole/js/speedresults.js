f7da7d847f7da94c3ffe147421ebdb6e0c8c395b
ipitio <21136719+ipitio@users.noreply.github.com>
2024-02-08 15:10:27 -0500

    moment

--- scripts/pi-hole/js/speedresults.js
+++ scripts/pi-hole/js/speedresults.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
       {
         render: function (data, type, _full, _meta) {
           if (type === "display") {
-            moment().utc(data).local();
+            moment.utc(data).local();
           }
 
           return data;
@@ -58,7 +58,7 @@ $(document).ready(function () {
       {
         render: function (data, type, _full, _meta) {
           if (type === "display") {
-            moment().utc(data).local();
+            moment.utc(data).local();
           }
 
           return data;
--- scripts/pi-hole/js/speedtest.js
+++ scripts/pi-hole/js/speedtest.js
@@ -129,18 +129,14 @@ function createChart() {
 }
 
 function formatDate(itemdate, results) {
-  let output = "HH:mm";
-  if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
-    return moment().utc(itemdate).local().format(output);
-  }
-
   const first = moment(results[0].start_time);
   const last = moment(results.at(-1).start_time);
+  let output = "HH:mm";
   if (last.diff(first, "hours") > 24) {
     output = "Do HH:mm";
   }
 
-  return moment().utc(itemdate).local().format(output);
+  return moment.utc(itemdate).local().format(output);
 }
 
 function updateSpeedTestData() {
