84ac3228172c51fd5a8a64ee77db2e2cb07bcd52
ipitio <21136719+ipitio@users.noreply.github.com>
2024-03-01 01:34:08 -0500

    fix footer

--- scripts/pi-hole/php/footer.php
+++ scripts/pi-hole/php/footer.php
@@ -97,8 +97,10 @@ if (isset($core_commit) || isset($web_commit) || isset($FTL_commit)) {
                 <p style="margin: 15px 0 0;">
                     <?php if ($docker_update) { ?>
                         To install updates, <a href="https://github.com/pi-hole/docker-pi-hole#upgrading-persistence-and-customizations" rel="noopener" target="_blank">replace this old container with a fresh upgraded image</a>.
-                    <?php } elseif ($core_update || $web_update || $FTL_update || $speedtest_update) { ?>
-                        To install updates, uninstall Speedtest Mod, run <code><a href="https://docs.pi-hole.net/main/update/" rel="noopener" target="_blank">pihole -up</a>, and reinstall Speedtest Mod. The Reinstall button in [ Settings > Speedtest ] can do this for you!</code>.
+                    <?php } elseif ($core_update || $web_update || $FTL_update) { ?>
+                        To install updates, uninstall Speedtest Mod, run <code><a href="https://docs.pi-hole.net/main/update/" rel="noopener" target="_blank">pihole -up</a></code>, and reinstall Speedtest Mod. The Reinstall button in [ Settings > Speedtest ] can do this for you!
+                    <?php } elseif ($speedtest_update) { ?>
+                        To install updates, reinstall Speedtest Mod. The Reinstall button in [ Settings > Speedtest ] can do this for you!
                     <?php } ?>
                 </p>
             </div>
